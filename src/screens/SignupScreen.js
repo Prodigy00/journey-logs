bc5af83b715c21f5c65495f8173a03df8424818d
Prodigy00 <simplygiddy@gmail.com>
2019-12-26 21:20:58 +0100

    styling sign up screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import { StyleSheet } from "react-native";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 40 }}>SignupScreen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
-        <Text>Go to Signin</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("mainFlow")}>
-        <Text>Go to Main flow</Text>
-      </TouchableOpacity>
+      <Text h3>Sign Up for Tracker</Text>
+      <Input label="Email" />
+      <Input label="Password" />
+      <Button title="Sign Up" onPress={() => navigation.navigate("Signin")} />
     </>
   );
 };
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-const TrackList = () => {
-  return <Text style={{ fontSize: 40 }}>TrackList</Text>;
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 40 }}>TrackListScreen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("TrackDetail")}>
+        <Text>Go to Track Detail</Text>
+      </TouchableOpacity>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackList;
+export default TrackListScreen;
