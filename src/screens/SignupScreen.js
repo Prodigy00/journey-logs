7682a771e3fbf92930b17aa1b5505fceab961d41
Prodigy00 <simplygiddy@gmail.com>
2019-12-26 22:30:51 +0100

    added state for sign up form

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Input, Button } from "react-native-elements";
 import { StyleSheet, View } from "react-native";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <Spacer>
         <Button title="Sign Up" onPress={() => navigation.navigate("Signin")} />
       </Spacer>
