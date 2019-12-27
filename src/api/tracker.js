fc0eb7d8f4977dc19e8cbd92bb97a41748758e9c
Prodigy00 <simplygiddy@gmail.com>
2019-12-27 21:52:07 +0100

    added safe area view

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ef6c57d6.ngrok.io"
+  baseURL: "http://ce022027.ngrok.io"
 });
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,16 +3,17 @@ import { Button } from "react-native-elements";
 import { Text, View, StyleSheet } from "react-native";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
-import AuthForm from "../components/AuthForm";
+import { SafeAreaView } from "react-navigation";
+
 const AccountScreen = () => {
   const { signOut } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text style={{ fontSize: 40 }}>AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signOut} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
