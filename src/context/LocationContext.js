bf2cdf03c1e6618f8c5e38d325eb9b8b114854c6
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 12:59:25 +0100

    added start and stop recording dispatch actions

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -1,18 +1,28 @@
 import createDataContext from "./createDataContext";
 
 const ADD_CURRENT_LOCATION = "ADD_CURRENT_LOCATION";
+const START_RECORDING = "START_RECORDING";
+const STOP_RECORDING = "STOP_RECORDING";
 const locationReducer = (state, action) => {
   switch (action.type) {
     case ADD_CURRENT_LOCATION:
       return { ...state, currentLocation: action.payload };
+    case START_RECORDING:
+      return { ...state, recording: true };
+    case STOP_RECORDING:
+      return { ...state, recording: false };
     default:
       return state;
   }
 };
 
-const startRecording = dispatch => () => {};
+const startRecording = dispatch => () => {
+  dispatch({ type: START_RECORDING });
+};
 
-const stopRecording = dispatch => () => {};
+const stopRecording = dispatch => () => {
+  dispatch({ type: STOP_RECORDING });
+};
 
 const addLocation = dispatch => location => {
   dispatch({ type: ADD_CURRENT_LOCATION, payload: location });
