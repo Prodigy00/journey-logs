bec8fa7daced8874fd38775489757e3ea535ce7e
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 18:30:03 +0100

    added fetch_tracks action

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -5,12 +5,17 @@ const FETCH_TRACKS = "FETCH_TRACKS";
 const CREATE_TRACK = "CREATE_TRACK";
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case FETCH_TRACKS:
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  const response = await trackerApi.get("/tracks");
+  dispatch({ type: FETCH_TRACKS, payload: response.data });
+};
 const createTrack = dispatch => async (name, locations) => {
   await trackerApi.post("/tracks", { name, locations });
 };
