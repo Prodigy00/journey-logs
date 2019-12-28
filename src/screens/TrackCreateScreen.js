ba6cc586919f7b632896c86affa26cd3e4fd7e2f
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 09:16:03 +0100

    initialized mock location test file

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import { requestPermissionsAsync } from "expo-location";
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy
+} from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreate = () => {
@@ -11,6 +15,16 @@ const TrackCreate = () => {
   const startWatching = async () => {
     try {
       await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        location => {
+          console.log({ location });
+        }
+      );
     } catch (error) {
       //   console.log({ err: error });
       SetErr(error);
