d9cbd1455ecb756a3b915a11ee0590be8162ec18
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 09:38:00 +0100

    created location context

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 import { setNavigator } from "./src/navigationRef";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
 
 const switchNavigator = createSwitchNavigator(
   {
@@ -37,12 +38,14 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App
-        ref={navigator => {
-          setNavigator(navigator);
-        }}
-      />
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App
+          ref={navigator => {
+            setNavigator(navigator);
+          }}
+        />
+      </AuthProvider>
+    </LocationProvider>
   );
 };
---
+++ src/context/LocationContext.js
@@ -0,0 +1,24 @@
+import createDataContext from "./createDataContext";
+
+const locationReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const startRecording = dispatch => () => {};
+
+const stopRecording = dispatch => () => {};
+
+const addLocation = dispatch => () => {};
+
+export const { Provider, Context } = createDataContext(
+  locationReducer,
+  { startRecording, stopRecording, addLocation },
+  {
+    recording: false,
+    locations: [],
+    currentLocation: null
+  }
+);
