6c95772039e73491c29f7debd35abc187ea7c294
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 10:17:53 +0100

    added position circle to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Text, StyleSheet, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackMap = () => {
@@ -19,12 +19,14 @@ const TrackMap = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-      }}
-    ></MapView>
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158,158,255,1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
