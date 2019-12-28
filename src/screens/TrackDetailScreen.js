cfa86008cac73cf59cfe5981e23eb2f02e095827
Prodigy00 <simplygiddy@gmail.com>
2019-12-28 19:44:55 +0100

    pulled track from state for track detail

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context as TrackContext } from "../context/TrackContext";
 import { Text, View, StyleSheet } from "react-native";
 
-const TrackDetail = () => {
-  return <Text style={{ fontSize: 40 }}>TrackDetail</Text>;
+const TrackDetail = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam("_id");
+
+  const track = state.find(trk => trk._id === _id);
+  return <Text style={{ fontSize: 40 }}>{track.name}</Text>;
 };
 
 const styles = StyleSheet.create({});
